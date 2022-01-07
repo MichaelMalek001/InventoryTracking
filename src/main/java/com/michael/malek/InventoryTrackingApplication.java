8c35a3b6676c2eaa4b1451033688ee4ef66c1ef7
Michael Malek <malek.mjm2@gmail.com>
2022-01-07 12:54:59 -0500

    Initialize repository

---
+++ src/main/java/com/michael/malek/InventoryTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.michael.malek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTrackingApplication.class, args);
+	}
+
+}
