06fe76dc31f990f778598910c563b708d3845ba7
Michael Malek <malek.mjm2@gmail.com>
2022-01-08 13:14:40 -0500

    implemented put and delete mappings

--- src/main/java/com/michael/malek/ProductController.java
+++ src/main/java/com/michael/malek/ProductController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,11 +23,21 @@ public class ProductController {
 	
 	@PostMapping(value = "/addProduct", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<?> addImage(@RequestParam("file") MultipartFile file, @RequestParam String name, @RequestParam double price, @RequestParam String description) {
-		return productService.saveImage(file, name, price, description);
+		return productService.saveProduct(file, name, price, description);
 	}
 
 	@GetMapping("/getAllProducts")
 	public ResponseEntity<List<FileResource>> giveAllProducts(){
-		return productService.getAllImages();
+		return productService.getAllProducts();
+	}
+	
+	@DeleteMapping("/deleteProduct/{id}")
+	public ResponseEntity<String> deleteProduct(@PathVariable int id) {
+		return productService.deleteProductById(id);
+	}
+	
+	@PutMapping("/editProduct")
+	public ResponseEntity<String> putImage(@RequestParam int id, @RequestParam String name, @RequestParam double price, @RequestParam String description) {
+		return productService.updateProduct(id, name, price, description);
 	}
 }
--- src/main/java/com/michael/malek/ProductService.java
+++ src/main/java/com/michael/malek/ProductService.java
@@ -1,5 +1,6 @@
 package com.michael.malek;
 
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -15,7 +17,7 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repo;
 	
-	public ResponseEntity<?> saveImage(MultipartFile file, String name, double price, String description) {
+	public ResponseEntity<?> saveProduct(MultipartFile file, String name, double price, String description) {
 		boolean fileIsValid = Utils.isValidFile(file);
 		if (!fileIsValid) {
 			return ResponseEntity.badRequest().body("Invalid file type.");
@@ -33,10 +35,10 @@ public class ProductService {
 		product.setDescription(description);
 		product.setFilePath(fileLocation);
 		repo.save(product);
-		return ResponseEntity.ok("File Uploaded Successfully");
+		return ResponseEntity.ok("Product Uploaded Successfully");
 	}
 	
-	public ResponseEntity<List<FileResource>> getAllImages(){
+	public ResponseEntity<List<FileResource>> getAllProducts(){
 		List<Product> allProducts =  repo.findAll();
 		List<FileResource> allFileResources = new LinkedList<FileResource>(); 
 		for (Product product : allProducts) {
@@ -46,5 +48,36 @@ public class ProductService {
 		return ResponseEntity.ok().body(allFileResources);
 	}
 	
-
+	public ResponseEntity<String> deleteProductById(int id) {
+		Product product = repo.findById(id).orElse(null);
+		if(product!=null) {
+			// Delete image file from folder
+			File fileToDelete = new File(product.getFilePath());
+			try {
+				fileToDelete.delete();
+			} catch (Exception e) {
+				return ResponseEntity.internalServerError().body(e.getMessage());
+			}
+			// Delete image file from database
+			repo.deleteById(id);
+			return 	ResponseEntity.ok().body("Product with id " + id + " has been removed.");
+		}
+		else {
+			return ResponseEntity.badRequest().body("Product with id " + id + " does not exist.");
+		}
+	}
+	
+	public ResponseEntity<String> updateProduct(int id, String name, double price, String description) {
+		Product newProduct = repo.findById(id).orElse(null);
+		
+		// check if product id given is valid
+		if(newProduct==null) {
+			return ResponseEntity.badRequest().body("Invalid request.");
+		}
+		newProduct.setName(name);
+		newProduct.setPrice(price);
+		newProduct.setDescription(description);
+		repo.save(newProduct);
+		return 	ResponseEntity.ok().body("Product with id " + newProduct.getId() + " has been updated.");
+	}
 }
